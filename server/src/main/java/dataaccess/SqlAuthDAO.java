c2a6f60a789dceeeb357b1411f1152638ff0e9dc
adammharris <harrisadam42103@gmail.com>
2024-11-04 13:37:04 -0700

    Abstract Sql access from SqlAuthDAO.java to SqlDAO.java
    Signed-off-by: adammharris <harrisadam42103@gmail.com>

--- server/src/main/java/dataaccess/SqlAuthDAO.java
+++ server/src/main/java/dataaccess/SqlAuthDAO.java
@@ -5,7 +5,6 @@ import model.AuthData;
 import java.sql.SQLException;
 import java.util.UUID;
 
-//TODO: abstract SQL access into SqlDAO
 public class SqlAuthDAO extends SqlDAO implements AuthDAO {
     private static SqlAuthDAO instance;
     private SqlAuthDAO() {
@@ -13,6 +12,7 @@ public class SqlAuthDAO extends SqlDAO implements AuthDAO {
     }
 
     private static void setupTable() {
+        tableName = "auths";
         try (var conn = DatabaseManager.getConnection()) {
             var statement = "CREATE TABLE IF NOT EXISTS auths (authToken VARCHAR(36), username VARCHAR(128), auth JSON)";
             try (var preparedStatement = conn.prepareStatement(statement)) {
@@ -33,52 +33,19 @@ public class SqlAuthDAO extends SqlDAO implements AuthDAO {
     @Override
     public AuthData createAuth(String username) {
         AuthData newAuth = new AuthData(UUID.randomUUID().toString(), username);
-        try (var conn = DatabaseManager.getConnection()) {
-            var auth = gson.toJson(newAuth);
-            var statement = "INSERT INTO auths (authToken, username, auth) VALUES ('%s', '%s', '%s')".formatted(newAuth.authToken(), username, auth);
-            try (var preparedStatement = conn.prepareStatement(statement)) {
-                preparedStatement.executeUpdate();
-            }
-        } catch (SQLException | DataAccessException e) {
-            throw new RuntimeException(e);
-        }
+        var auth = gson.toJson(newAuth);
+        create(new String[]{"authToken", "username", "auth"}, new String[]{newAuth.authToken(), username, auth});
         return newAuth;
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
-        if (authToken == null) {
-            throw new DataAccessException("Error: Not found");
-        }
-        AuthData auth = null;
-        try (var conn = DatabaseManager.getConnection()) {
-            var statement = "SELECT authToken, auth FROM auths WHERE authToken='%s'".formatted(authToken); //TODO: String can be unsafe?
-            try (var preparedStatement = conn.prepareStatement(statement)) {
-                try (var result = preparedStatement.executeQuery()) {
-                    if (result.next()) {
-                        auth = gson.fromJson(result.getString("auth"), AuthData.class);
-                    }
-                }
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        if (auth == null) {
-            throw new DataAccessException("Error: Not found");
-        }
-        return auth;
+        return get("authToken", authToken, "auth", AuthData.class);
     }
 
     @Override
     public void deleteAuth(String authToken) throws DataAccessException {
-        try (var conn = DatabaseManager.getConnection()) {
-            var statement = "DELETE FROM auths WHERE authToken='%s'".formatted(authToken); //TODO: String can be unsafe?
-            try (var preparedStatement = conn.prepareStatement(statement)) {
-                preparedStatement.executeUpdate();
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        delete("authToken", authToken);
     }
 
     public String getUsername(String authToken) throws DataAccessException {
@@ -87,17 +54,5 @@ public class SqlAuthDAO extends SqlDAO implements AuthDAO {
             return auth.username();
         }
         throw new DataAccessException("Error: Unauthorized");
-    }
-
-    @Override
-    public void clear() {
-        try (var conn = DatabaseManager.getConnection()) {
-            var statement = "TRUNCATE auths";
-            try (var preparedStatement = conn.prepareStatement(statement)) {
-                preparedStatement.executeUpdate();
-            }
-        } catch (SQLException | DataAccessException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
--- server/src/main/java/dataaccess/SqlDAO.java
+++ server/src/main/java/dataaccess/SqlDAO.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 
 public abstract class SqlDAO {
     protected static Gson gson = new Gson();
+    protected static String tableName;
 
     static {
         try {
@@ -17,7 +18,7 @@ public abstract class SqlDAO {
 
     public void clear() {
         try (var conn = DatabaseManager.getConnection()) {
-            var statement = "TRUNCATE auths"; //TODO: What is a proper abstraction of clear?
+            var statement = "TRUNCATE " + tableName; //TODO: String can be unsafe?
             try (var preparedStatement = conn.prepareStatement(statement)) {
                 preparedStatement.executeUpdate();
             }
@@ -26,7 +27,58 @@ public abstract class SqlDAO {
         }
     }
 
-    // TODO: abstract SQL access
+    public static void delete(String key, String value) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "DELETE FROM %s WHERE %s='%s'".formatted(tableName, key, value); //TODO: String can be unsafe?
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    public static void create(String[] keys, String[] values) {
+        String statement = "INSERT INTO %s (".formatted(tableName);
+        for (String key : keys) {
+            statement = statement.concat(key + ", ");
+        }
+        statement = statement.substring(0, statement.length()-2).concat(") VALUES (");
+        for (String value : values) {
+            statement = statement.concat("'" + value + "', ");
+        }
+        statement = statement.substring(0, statement.length()-2).concat(")");
+        // TODO: String can be unsafe?
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    public static <T> T get(String keyIdentifier, String valueIdentifier, String keyGet, Class<T> classOfT) throws DataAccessException {
+        if (keyIdentifier == null) {
+            throw new DataAccessException("Error: Bad request");
+        }
+        T rec = null;
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT %s, %s FROM %s WHERE %s='%s'".formatted(keyIdentifier, keyGet, tableName, keyIdentifier, valueIdentifier);
+            //TODO: String can be unsafe?
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                try (var result = preparedStatement.executeQuery()) {
+                    if (result.next()) {
+                        rec = gson.fromJson(result.getString(keyGet), classOfT);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        if (rec == null) {
+            throw new DataAccessException("Error: Not found");
+        }
+        return rec;
+    }
 }
