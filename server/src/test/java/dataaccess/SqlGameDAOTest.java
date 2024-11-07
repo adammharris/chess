7052c848b80692bd92765dcb9bb3eab5c15c942c
adammharris <harrisadam42103@gmail.com>
2024-11-06 19:46:24 -0700

    Stub tests for SqlGameDAO and SqlUserDAO
    Signed-off-by: adammharris <harrisadam42103@gmail.com>

--- server/src/test/java/dataaccess/SqlGameDAOTest.java
+++ server/src/test/java/dataaccess/SqlGameDAOTest.java
@@ -1,20 +1,42 @@
 package dataaccess;
 
+import model.GameData;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SqlGameDAOTest {
-
+    private final static SqlGameDAO gameDAO = SqlGameDAO.getInstance();
+    @BeforeAll
+    static void setup() {
+        gameDAO.clear();
+    }
     @Test
     void createGame() {
+        assertDoesNotThrow(() -> gameDAO.createGame("gameName"));
     }
 
     @Test
     void getGame() {
+        final GameData result;
+        try {
+            result = gameDAO.createGame("gameName");
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        assertDoesNotThrow(() -> gameDAO.getGame(result.gameID()));
     }
 
     @Test
     void updateGame() {
+        final GameData result;
+        try {
+            result = gameDAO.createGame("gameName");
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        assertDoesNotThrow(() -> gameDAO.updateGame(new GameData(result.gameID(), "new", "name", result.gameName(), result.game())));
+
     }
 }
--- server/src/test/java/dataaccess/SqlUserDAOTest.java
+++ server/src/test/java/dataaccess/SqlUserDAOTest.java
@@ -1,16 +1,26 @@
 package dataaccess;
 
+import model.UserData;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SqlUserDAOTest {
-
+    private final static SqlUserDAO userDAO = SqlUserDAO.getInstance();
     @Test
     void createUser() {
+        assertDoesNotThrow(() -> userDAO.createUser(new UserData("a", "b", "c")));
     }
 
     @Test
     void getUser() {
+        final UserData result;
+        try {
+            userDAO.createUser(new UserData("a", "b", "c"));
+            result = userDAO.getUser(new UserData("a", "b","c"));
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        assertNotNull(result);
     }
 }
