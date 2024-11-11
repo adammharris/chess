50fd8e2fcb2e230d63e5c2ed92ad3155170fba74
adammharris <harrisadam42103@gmail.com>
2024-11-11 16:17:48 -0700

    Move prelogin logic to its own class. Implemented help command
    Signed-off-by: adammharris <harrisadam42103@gmail.com>

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,13 @@
 import chess.*;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        //var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        System.out.println("♕ Welcome to 240 Chess. Type 'help' to get started.");
+
+        Prelogin prelogin = new Prelogin(new Scanner(System.in));
+        prelogin.inputCommand();
     }
 }
---
+++ client/src/main/java/Prelogin.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Prelogin {
+    Scanner scanner;
+    public Prelogin(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void inputCommand() {
+        boolean keepGoing = true;
+        while (keepGoing) {
+            System.out.print("[LOGGED OUT] >>> ");
+            String input = scanner.next();
+            switch (input) {
+                case "help":
+                    System.out.print("""
+                            Command\t\tDescription
+                            Help\t\tDisplays list of possible commands.
+                            Quit\t\tExits the program.
+                            Login\t\tAsks for login information and grants access to chess games.
+                            Register\tAsks for registration information, then logs in.
+                            """);
+                    break;
+                case "quit":
+                    System.out.print("Goodbye!\n");
+                    keepGoing = false;
+                    break;
+                case "register":
+                    System.out.print("Time to register! (Not implemented)\n");
+                    break;
+                case "login":
+                    System.out.print("Enter credentials: (Not implemented)\n");
+                    break;
+
+            }
+        }
+    }
+}
