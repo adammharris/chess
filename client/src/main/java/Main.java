b87865b6889efd2c9a1709f962557e331c12dd9f
adammharris <harrisadam42103@gmail.com>
2024-11-14 18:53:12 -0700

    Hopefully compiles in autograder
    Signed-off-by: adammharris <harrisadam42103@gmail.com>

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,9 +1,10 @@
 import ui.Game;
 import java.util.Scanner;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
-        server.Server server = new server.Server();
+        Server server = new Server();
         int port = server.run(8080);
         Game.start(port, new Scanner(System.in));
         server.stop();
