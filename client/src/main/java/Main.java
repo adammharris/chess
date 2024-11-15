ce2cc45db2c54fb5fa07c01dfd5d6fdd086c69bd
adammharris <harrisadam42103@gmail.com>
2024-11-14 18:59:28 -0700

    Server and client are separate
    Signed-off-by: adammharris <harrisadam42103@gmail.com>

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,12 +1,8 @@
 import ui.Game;
 import java.util.Scanner;
-import server.Server;
 
 public class Main {
     public static void main(String[] args) {
-        Server server = new Server();
-        int port = server.run(8080);
-        Game.start(port, new Scanner(System.in));
-        server.stop();
+        Game.start(8080, new Scanner(System.in));
     }
 }
