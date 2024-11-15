3ed60038f209d5e041d0586b60c711a928b172e7
adammharris <harrisadam42103@gmail.com>
2024-11-14 18:35:07 -0700

    Fixed module independence?
    Signed-off-by: adammharris <harrisadam42103@gmail.com>

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,10 +1,9 @@
-import server.Server;
 import ui.Game;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Server server = new Server();
+        server.Server server = new server.Server();
         int port = server.run(8080);
         Game.start(port, new Scanner(System.in));
     }
